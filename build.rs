aadace34b855757e956585d8599bf49ec26346c5
mirko-geno <mirkogenovesi24@gmail.com>
2026-09-20 01:14:21 -0300

    Translate onnx model to Burn

---
+++ build.rs
@@ -0,0 +1,24 @@
+use burn_onnx::ModelGen;
+// For saving the generated files to the project
+/*
+use std::env;
+
+fn main(){
+    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let model_dir = format!("{project_dir}/model/");
+    let onnx_path = format!("{model_dir}/sleep_person_yolo26n.onnx");
+        
+
+    ModelGen::new()
+        .input(&onnx_path)
+        .out_dir(&model_dir)
+        .run_from_script();
+}
+*/
+fn main() {
+    ModelGen::new()
+        .input("model/sleep_person_yolo26n.onnx")
+        .out_dir("model/")
+        .run_from_script();
+}
+
